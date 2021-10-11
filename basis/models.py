541809caedba808de56be3184101f671cad6fb2a
Tatiana Smatova <smatova.tatiana@gmail.com>
2021-10-11 23:21:48 +0300

    Iteration 2. Create Education model in models.py Register model in admin.py Add 3 courses in DB

--- basis/models.py
+++ basis/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Education(models.Model):
+    graduated = models.PositiveSmallIntegerField()
+    school_name = models.CharField(max_length=255)
+    course_name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    certificate = models.ImageField(blank=True)
+    slug = models.SlugField(max_length=255, unique='course_name')
+
+    def __str__(self):
+        return self.course_name
