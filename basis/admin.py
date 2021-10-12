16275ded00cffd79de9ea8d76158971d272293a8
Tatiana Smatova <smatova.tatiana@gmail.com>
2021-10-12 16:19:45 +0300

    Iteration 5. Ad  basic settings of the Admin for Education model/ prepopulated fields, ordering, list_display

--- basis/admin.py
+++ basis/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
-admin.site.register(models.Education)
+#admin.site.register(models.Education)
+
+
+@admin.register(models.Education)
+class EducationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('course_name', )}
+    list_display = ('course_name', 'graduated', 'school_name')
+    ordering = ('graduated', 'school_name')
+    list_filter = ('graduated', 'school_name')
