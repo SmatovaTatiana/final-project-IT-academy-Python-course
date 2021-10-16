760f42580cd59a3b37642ca2f55011426c5fc16b
Tatiana Smatova <smatova.tatiana@gmail.com>
2021-10-16 15:28:45 +0300

    Iteration 15. Add reset password functionality

--- basis/urls.py
+++ basis/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+
+
+class MyHackedView(auth_views.PasswordResetView):
+    success_url = reverse_lazy('basis:password_reset_done')
 
 app_name = 'basis'
 urlpatterns = [
@@ -13,13 +17,23 @@ urlpatterns = [
     path('all_experience/', views.all_experience, name='all_experience'),
     path('<slug:slug>/<str:company_name>/<str:position>', views.detailed_experience,
          name='detailed_experience'),
+
     path('contact_form/', views.contact_form, name="contact_form"),
     path('upload_documents/', views.document_form_upload, name='upload_documents'),
+
     path('all_portfolio/', views.all_portfolio, name='all_portfolio'),
     path('<slug:slug>/<str:project_name>', views.detailed_portfolio, name='detailed_portfolio'),
 #    path('login/', views.custom_login, name='login'),
+
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('password_reset/', MyHackedView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('basis:password_reset_complete'),
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
 if settings.DEBUG:
