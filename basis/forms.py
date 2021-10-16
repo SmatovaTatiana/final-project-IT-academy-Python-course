1d6655abf73a35491975f247b9d99e66bafa36af
Tatiana Smatova <smatova.tatiana@gmail.com>
2021-10-16 16:50:20 +0300

    Iteration 17. Add registration functionality

--- basis/forms.py
+++ basis/forms.py
@@ -19,3 +19,18 @@ class DocumentForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Pass', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Pass2', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('bad password')
+        return cd['password']
--- basis/urls.py
+++ basis/urls.py
@@ -38,6 +38,8 @@ urlpatterns = [
 
     path('profile/', views.profile, name='profile'),
 
+    path('register/', views.register, name='register'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
