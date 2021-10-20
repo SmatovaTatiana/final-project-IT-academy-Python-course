e7eb33dd50544e668d88bb2d9c3ebb866cb57e0d
Tatiana Smatova <smatova.tatiana@gmail.com>
2021-10-20 17:50:08 +0300

    Iteration 24. Add news page

--- basis/urls.py
+++ basis/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
 
     path('register/', views.register, name='register'),
 
-
+    path('news/', views.all_news, name='all_news'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
